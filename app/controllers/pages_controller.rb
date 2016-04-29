10ac7aa7ad8112dbdaa7bd7d97262e97866664b4
davycro <me@davecro.com>
2016-04-28 18:03:51 -0600

    primitive styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,11 @@ class PagesController < ApplicationController
   end
 
   def schedule
-    d = Date.new(params[:date][:year].to_i, params[:date][:month].to_i, params[:date][:day].to_i)
+    if params[:date].blank?
+      d = Date.today
+    else
+      d = Date.new(params[:date][:year].to_i, params[:date][:month].to_i, params[:date][:day].to_i)
+    end
     @schedule = Schedule.new d
   end
 end
